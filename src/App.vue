fc3d77117c04790d239e381487f9e74922ca7d90
Paula <paula@pena-parra.ch>
2023-11-14 15:56:23 +0100

    connection a la base de données

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,19 @@
 <script setup>
 // Cheat Sheet: https://steve-fallet.notion.site/Vue-3-script-setup-Cheat-Sheet-b12192ceae244ecda65f771579ca02bc
-import {ref} from 'vue'
+import {ref, onMounted} from 'vue'
 
 // Tableau des troupes
-const troupes = ref([
-  {
-    "id": 2,
-    "nom": "Archer",
-    "niveau": 3,
-    "formation": 25,
-    "vitesse": 24,
-    "cout": 200,
-    "description": "Les archers sont des tireurs d'élite qui attaquent à distance. Ils sont rapides et bon marché à former, mais ils sont faibles en mêlée et doivent être protégés.",
-    "couleur": "#EE5487",
-    "image": "https://cocapi.divtec.me/img/archer.png"
-  },
-  {
-    "id": 1,
-    "nom": "Barbare",
-    "niveau": 4,
-    "formation": 20,
-    "vitesse": 16,
-    "cout": 150,
-    "description": "Ce guerrier intrépide compte sur ses muscles saillants et sa fière moustache pour semer le chaos dans les villages ennemis. Faites charger une horde de barbares et profitez du spectacle !",
-    "couleur": "#ebb428",
-    "image": "https://cocapi.divtec.me/img/barbare.png"
-  },
-  {
-    "id": 3,
-    "nom": "Géant",
-    "niveau": 5,
-    "formation": 12,
-    "vitesse": 12,
-    "cout": 2250,
-    "description": "Les géants sont de grands guerriers qui attaquent les défenses ennemies en premier. Ils sont lents mais résistants.",
-    "couleur": "#F6901A",
-    "image": "https://cocapi.divtec.me/img/geant.png"
-  }
-])
+const troupes = ref([])
+
+// Cycle de vie du composant
+// Quand le composant est monté, on va chercher les données
+onMounted(() => {
+  fetch('https://cocapi.divtec.me/troupes')
+      .then((res) => res.json()) // Transforme le json en JavaScript
+      .then((data) => { // Récupère les données et les sauve dans le tableau
+        troupes.value = data
+      })
+})
 </script>
 
 <template>
@@ -79,7 +55,8 @@ const troupes = ref([
           </header>
           <div
               class="level"
-              :style="`color: ${troupe.couleur};`">
+              :style="`color: ${troupe.couleur};`"
+          >
             Niveau {{ troupe.niveau }}
           </div>
           <h2 class="name">{{ troupe.nom }}</h2>
