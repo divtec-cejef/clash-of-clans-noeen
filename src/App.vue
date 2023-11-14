685b2c4f39b478a68334228439690f60b36f9842
Paula <paula@pena-parra.ch>
2023-11-14 16:35:57 +0100

    composante TroupeCarte.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 import {ref, onMounted} from 'vue'
 import PageFooter from "@/components/PageFooter.vue";
 import PageTopBar from "@/components/PageTopBar.vue";
+import TroupeCarte from "@/components/TroupeCarte.vue";
 
 // Tableau des troupes
 const troupes = ref([])
@@ -53,53 +54,7 @@ function formerTroupe(troupe) {
           v-for="troupe in troupes"
           :key="troupe.id"
       >
-        <article>
-          <header
-              :style="`background: linear-gradient(60deg,#3B3B3B 0%, ${troupe.couleur} 100%);`"
-          >
-            <img
-                :src="troupe.image"
-                :alt="troupe.nom"
-            >
-          </header>
-          <div
-              class="level"
-              :style="`color: ${troupe.couleur};`"
-          >
-            Niveau {{ troupe.niveau }}
-          </div>
-          <h2 class="name">{{ troupe.nom }}</h2>
-          <button
-              :style="`background-color: ${troupe.couleur};`"
-              @click="formerTroupe"
-          > Former
-            <img src="/img/piece-or.png" alt="Former">
-          </button>
-          <p class="description">{{ troupe.description }}</p>
-          <footer>
-            <div
-                class="training"
-                :style="`background-color: ${troupe.couleur};`"
-            >
-              <div>{{ troupe.formation }}<sup>sec</sup></div>
-              <div>Formation</div>
-            </div>
-            <div
-                class="speed"
-                :style="`background-color: ${troupe.couleur};`"
-            >
-              <div>{{ troupe.vitesse }}</div>
-              <div>Vitesse</div>
-            </div>
-            <div
-                class="cost"
-                :style="`background-color: ${troupe.couleur};`"
-            >
-              <div>{{ troupe.cout }}</div>
-              <div>Coût</div>
-            </div>
-          </footer>
-        </article>
+        <troupe-carte :troupe="troupe" />
       </li>
     </ul>
   </main>
